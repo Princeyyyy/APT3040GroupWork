181a78226e921c95a51297d86be3bea369897885
Princeyyyy <princemumo80@gmail.com>
2024-03-21 06:07:11 +0300

    finished two pages

---
+++ src/project_package/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project_package;
+
+/**
+ *
+ * @author princ
+ */
+public class Main {
+    public static void main(String[] args) {
+        loginPage login = new loginPage();
+        login.show();
+    }
+}
